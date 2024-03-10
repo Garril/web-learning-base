2261123f21ab408f2ce3535cd4eceea42292673a
Garril <CoderGarril@outlook.com>
2024-03-10 15:24:59 +0800

    fix(add circlescroll): add a small rotating icon on the page

--- accumulate/minimodule/scroll/parallax_scrolling/index.js
+++ accumulate/minimodule/scroll/parallax_scrolling/index.js
@@ -1,7 +1,11 @@
 const sections = document.querySelectorAll('section');
+const circleEle = document.querySelector('.my-circle');
+
 
 try {
+  // 插件注册
   gsap.registerPlugin(ScrollTrigger);
+  // 视差滚动
   sections.forEach(section => {
     gsap.fromTo(section, {
       backgroundPositionY: `-${window.innerHeight / 2}px`,
@@ -14,6 +18,21 @@ try {
         scrub: true, // 与时间脱钩，和滚动关联
       }
     })
+  })
+  // 圆圈位移(随时间控制，不关联滚动)
+  gsap.fromTo(circleEle, {
+    x: 0,
+    rotate: 0,
+  }, {
+    x: function (eleIndex, target) {
+      return document.documentElement.clientWidth - target.offsetWidth
+    },
+    y: function (eleIndex, target) {
+      return document.documentElement.clientHeight - target.offsetHeight
+    },
+    rotate: 360,
+    duration: 3,
+    ease: 'none'
   })
 } catch (e) {
   console.log(e);
